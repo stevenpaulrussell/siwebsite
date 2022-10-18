8bbff74aff6f7d66dd858a77a742f912e0dcbb2f
Steve <steven_russell@hotmail.com>
2022-10-17 17:59:40 -0700

    Have better structure in twitalk view and in twitalk data_dict

--- twitalk/data_dict.py
+++ twitalk/data_dict.py
@@ -3,36 +3,22 @@
 
 # senders
 """
-f'{from_tel}':                # These are written by postmaster, read by twitalk.  It is the whole state for the sender!
-    'status': free_tier
-    f'from_{from_tel}': -> sender name if key is present        # Use to customize sms to sender
-    f'to_{to_tel}:  -> recipient name if key is present         # Use to customize sms to sender
+f'free_tier/{from_tel}':                # These are written by postmaster, read by twitalk.
+    f'{to_tel}':  
+        'from': -> sender name if key 'from' is present        # Use to customize sms to sender
+        'to': -> recipient name if key is present         # Use to customize sms to sender
 
 
-f'{from_tel}':               # These are written by twitalk, read by twitalk, deleted by postmaster
-        'status': -> need_image, need_audio, need_profile, free_tier     # Simplify code by specifying what is acceptable
-        'begun_at': -> time.time()      # When first heard from.  Pruned by postmaster if not completed within xx hours     
-        'finished_at': -> time.time()   # When profile received.  This allows twitalk to process a new postcard immediately      
-        'to_tel': -> to_tel             # Freeze any new to_tel until postmaster rewrites to free_tier
-        'image_url': -> image media_url
-        'image_hint': -> image media_hint
-        'audio_url': -> audio media_url
-        'audio_hint': -> audio media_hint
-        'profile_url': -> profile image media_url
-        'profile_hint': -> profile image media_hint
+f'new_sender/{from_tel}':               # These are written by postmaster, read by twitalk, deleted by postmaster
+        'expect': -> image, audio, profile     # Simplify code by specifying what is acceptable
+
 """
 
 # wip     
 """
-f'wip/{from_tel}__to__{to_tel}'    # Written, read, and deleted by twitalk for state store
-    'image': 
-        'post_time': -> time.time() when image stored in cards_in_progress 
-        'media_hint': 
-        'media_url':
-    'audio': 
-        'post_time': 
-        'media_hint': 
-        'media_url':       
+f'wip/{from_tel}/{to_tel}'    # Written, read, and deleted by twitalk for state store
+    'image_timestamp': 
+    'image_url':
 """
 
 # command SQS.  Includes explicit commands from sms and implicit commands, for now only 'new_sender'
--- twitalk/views.py
+++ twitalk/views.py
@@ -18,74 +18,42 @@ DEFAULT_TWILIO_NUMBER = '+18326626430'
 
 # Entry points to get media from Twilio
 @csrf_exempt
-def accept_media(request):      # mms entry point 
+def accept_media(request):      # mms entry point, image only for now!!
     postdata = request.POST
     from_tel = postdata['From']
     to_tel = postdata['To']
-    time_stamp = time.time() - float(postdata.get('test_time_shift', '0'))
+    timestamp = time.time() - float(postdata.get('test_timeshift', '0'))
     text = postdata.get('Body', '').lower().strip()
     media_type = postdata.get('MediaContentType0', 'no_media/no_media').split('/')[0]
-    media_url = postdata.get('MediaUrl0', None)
-
-    sender = filerviews.load_from_free_tier(from_tel, to_tel)
+    image_url = postdata['MediaUrl0'] if media_type == 'image' else None 
+    sender, wip = filerviews.load_from_free_tier(from_tel, to_tel)
     if sender:
-        msg = process_free_tier(sender, request)
-        return msg
+        if image_url and not text:
+            """Put image into wip"""
+        elif image_url and text == 'profile':
+            """Send profile command SQS pre-processor"""
+        elif not image_url and text:
+            """Call command pre-processor. If it is help, send directly, otherwise appeal to postmaster"""
+        return 
     else:
-        sender = filerviews.load_from_new_senders(from_tel)   # filerviews will write a new sender if one is not found!
-        msg = process_new_sender(sender, request)
-        return msg
+        expect = filerviews.load_from_new_senders(from_tel)   
+        match expect:
+            case 'image':
+                if image_url and not text:
+                    """Send new_sender_image on SQS"""
+                else:
+                    """Send request for first image and link to instructions.  Make an error SQS for mgmt??"""
+            case 'audio':
+                """Send some instruction back to call the number, link to instructions.   Make an error SQS for mgmt??"""
+            case 'profile':
+                if image_url and text == 'profile':
+                    """Send new_sender_profile on SQS and send welcome message"""
+                else:
+                    """Send instruction on profile and link to instructions.  Make an error SQS for mgmt??"""
+            case _:
+                """Raise some error"""
 
 
-def process_free_tier(sender, request):
-    pass
-
-def process_new_sender(sender, request):
-    pass
-
-    
- 
-    try: 
-        
-    except filerexceptions.S3KeyNotFound:
-        sender = dict(status='need_image', begun_at=time_stamp, to_tel=to_tel)
-        filerviews.save_new_sender_state(from_tel=from_tel, state=sender)
-    status = sender['status']
-    match status:
-        case 'free_tier':
-            pass
-        case 'need_image':
-            pass 
-        case 'need_audio':
-            pass 
-        case 'need_profile':
-            pass 
-        case 'image_needed':
-            pass 
-        case 'image_needed':
-            pass 
-
-
-
-
-
-    if request.method!='POST':
-        
-    try:
-        sender, to_tel, time_stamp = get_state_setup(request.POST)
-        message, text, media_type, media_hint, media_url = mmscleaner(request.POST)
-        if not message:
-            # New below to be coded or changed, including sender key value fields
-            message = process_media(time_stamp, sender, to_tel, text, media_type, media_hint, media_url)  # Raises exception if NCD
-        save_one_sender_state(sender)
-        text_back_to_sender = f"{message} Your status: {sender['status']}"
-        return HttpResponse(content=f"<Response><Message>{text_back_to_sender}</Message></Response>")  
-    except Exception as E:
-        if os.environ['TEST'] == 'True':
-            raise
-        message = f'Have a problem: "{repr(E)}". Contact Steve'
-        sms_to_some_sender(message, '+16502196500')
-        return HttpResponse(content=f"<Response><Message>{message}</Message></Response>")  
 
 @csrf_exempt
 def twi_recorder(request):      # voice recording entry point
@@ -93,77 +61,36 @@ def twi_recorder(request):      # voice recording entry point
     The sms message declares success, next step, or program error. Program errors are from 
     the try/except wrapper. The twilio protocol is managed in mydata.voice_cleaner.  Program errors are from 
     the try/except wrapper.   Program error sends a message via 'sms_to_some_sender'."""
-
-    if request.method!='POST':
-        return HttpResponse(status=405)
-    try:
-        sender, to_tel, time_stamp = get_state_setup(request.POST)
-        http_back, media_url = voice_cleaner(request.POST, to_tel, sender)
-        if http_back:
-            return http_back  # 'Record' prompt 
-        else:
-            message = process_media(time_stamp, sender, to_tel, '', 'audio', 'audio/mp3', media_url)
-            save_one_sender_state(sender)
-            sms_to_some_sender(message, sender['from_tel'], twilio_number=to_tel)
-            return HttpResponse()
-    except Exception as E:
-        if os.environ['TEST'] == 'True':
-            raise
-        message = f'Have a voice twilio problem "{repr(E)}" with message from {sender["from_tel"]} to {to_tel}.'
-        sms_to_some_sender(message, '+16502196500')
-        return HttpResponse()  
-
-def process_media(time_stamp, sender, to_tel, text, media_type, media_hint, media_url):
-    if sender['status'] in ('free_tier', ) and not text:
-        message = process_regular_postcard(time_stamp, sender, to_tel, media_type, media_hint, media_url)
-        # if 'Postcard sent' in message:
-        #     send_the_new_card(time_stamp, sender)  # Use saved postcard id to update po_box & save po_box
-    elif sender['status'] in ('free_tier', ) and text:
-        message = process_possible_command(time_stamp, sender, to_tel, text, media_type, media_hint, media_url)
-    elif sender['status'] in ('starting', 'need_audio', 'need_profile'):     
-        message = process_new_sender_media(time_stamp, sender, to_tel, text, media_type, media_hint, media_url)
-    if not message:
-        status = f'sender: {sender}\n, to_tel: {to_tel} text: {text} media_type: {media_type}\n'
-        raise Exception(f'In process media(), have no message generated. State: {status}')
-    return message
-
-def get_state_setup(postdata):
+    postdata = request.POST
     from_tel = postdata['From']
     to_tel = postdata['To']
-    try:
-        time_stamp = time.time() - float(postdata['test_time_shift'])
-    except KeyError:
-        time_stamp = time.time()
-    try:
-        sender = load_one_sender_state(from_tel)
-    except:   # Catch some boto3 exception!!
-        sender = new_sender(from_tel, time_stamp)
-    new_recipient_check(from_tel, to_tel, sender)
-    return  sender, to_tel, time_stamp
-
-def new_sender(from_tel, time_stamp):
-    sender = dict(from_tel=from_tel, status='starting', profile_photo_url=None, name=from_tel, heard_from=time_stamp, connections={})
-    sms_to_some_sender(f'new_sender_check: {from_tel}', '+16502196500')
-    return sender
-
-def new_recipient_check(from_tel, to_tel, sender):
-    if to_tel not in sender['connections']:    
-        wip = {}
-        po_box_id = str(uuid.uuid4())
-        sender['connections'][to_tel] = dict(wip=wip, po_box_id=po_box_id, recipient_handle=to_tel) 
-        penpals = {from_tel: []}   
-        po_box = dict(po_box_id=po_box_id, penpals=penpals, key_operators=[from_tel,])
-        save_one_po_box(po_box)
-        sms_to_some_sender(f'{from_tel} using new recipient to_tel: {to_tel}', '+16502196500')
-
-def sms_to_some_sender(text_back_to_sender, senders_tel, twilio_number=DEFAULT_TWILIO_NUMBER):
-        if 'test' in senders_tel in twilio_number or os.environ['TEST'] == 'True':
-            if 'Error' in text_back_to_sender:
-                print(f'\n\nsms to {senders_tel}: {text_back_to_sender}\n\n')
+    timestamp = time.time() - float(postdata.get('test_timeshift', '0'))
+    audio_url = postdata.get('RecordingUrl', None)
+    if audio_url:
+        audio_url = audio_url + '.mp3' 
+    sender, wip = filerviews.load_from_free_tier(from_tel, to_tel)
+    if sender:
+        if  'image_url' in wip: 
+            if not audio_url:
+                """Make the recording prompt using the from and to parameters if they exist."""
+            else:
+                """Write the postcard sent SQS and clear the wip"""
         else:
-            account_sid = os.environ['TWILIO_ACCOUNT_SID']
-            auth_token = os.environ['TWILIO_AUTH_TOKEN']
-            client = twilioClient(account_sid, auth_token)
-            client.messages.create(to=senders_tel, from_=twilio_number, body=text_back_to_sender)
+            """ Handle both a prompt to send an image then call, and also a message back with instructions."""
+
+
+
+    # if sender:
+    #     if 'RecordingUrl' in postdata:  
+    #         return None,  postdata['RecordingUrl'] + '.mp3'
+    #     else:
+    #         sender_name = None if '+1' in sender['name'] else sender['name'] 
+    #         recipient_handle = sender['connections'][to_tel]['recipient_handle'] 
+    #         if '+1' in recipient_handle:
+    #             spoken = f'Hello {sender_name}. Have your photo.  Now tell your story about it, then just hang up.'
+    #         else:
+    #             spoken = f'Hello {sender_name}. Have your photo.  Now tell {recipient_handle} your story about it, then just hang up.'
+    #         twilio_cmd = f'<Say>{spoken}</Say><Record maxLength="100"/>'
+    #         return HttpResponse(content=f'<?xml version="1.0" encoding="UTF-8"?><Response>{twilio_cmd}</Response>'), None
 
 
