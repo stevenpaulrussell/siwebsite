a1a8b2f741f7149f952d326d850c04394d580599
Steve <steven_russell@hotmail.com>
2022-11-23 16:22:36 -0800

    Intermediate commit of postmaster.views first_postcard and new_postcard processing including writes and deletes to s3.  No tests written.

--- postmaster/data_dict.py
+++ postmaster/data_dict.py
@@ -2,49 +2,47 @@
 
 """
 f'sender/{from_tel}':
-    # {sender data structure}
-    # conn?
-    # most recent card (for each to_tel?)    'from_tel': from_tel
+    'version': -> start at 1
+    'from_tel' -> from_tel              # Serves as ID.  Make a uuid for the sender??
     'profile_photo_url': ->
-    'name': -> default to from_tel, change by text command
     'heard_from': -> time.time() of last sms, mms, or twilio
-    'connections':
+    'conn':
         to_tel: 
-            'wip': {}   #  'audio': -> dict(post_time=post_time, media_hint=media_hint, media_url=media_url)
-                        #   'image': -> dict(post_time=post_time, media_hint=media_hint, media_url=media_url)
-            'po_box_id': -> po_box_uuid  
-            'recent_card_id': -> uuid4_card_id
+            'from': -> -> default to from_tel, change by text command        # Used by twilio to customize sms to sender
+            'to': ->  default to to_tel, change by text command               # Used by twilio to customize sms to sender
+            'pobox_id': -> pobox_id  or None                        # None until viewer is connected for (from_tel, to_tel). 
+            'recent_card_id': -> card_id
             'recent_card_when': -> time_stamp
-            'recipient_handle' -> default to to_tel, change by text command  
 
 
 
-f'conn/{from_tel}/{to_tel}':
-    pobox_uuid or None                          # None until viewer is connected for (from_tel, to_tel). 
 
-
-f'pobox_{pobox_uuid}':
+f'pobox_{pobox_id}':
+    'version': -> start at 1
+    'id': pobox_id
     key_operator: f'{from_tel}              # set when viewer is first connected for (from_tel, to_tel)
-    'penpals': ->
-        f'{from_tel}:   [postcard_uuid,]        # for each from_tel, a list of postcards not yet archived
+    f'{from_tel}:   [postcard_id,]        # for each from_tel, a list of postcards not yet archived
     ...
 
     
     
-f'card_{postcard_uuid}':
-    # {postcard data structure}
-        'uuid':  -> uuid4_card_id, 
-        'plays':  -> default 0, then updated from Recently_Played_Cards_V1 when archived  ---> change this?                                  
-        'addressing': 
-            'from_tel':
-            'to_tel': 
-            'sent_at': -> time.time() when made and stored in S3 and referenced in post_office     
-        'image_url': 
-        'audio_url': 
+f'card_{postcard_id}':
+    'version': -> start at 1
+    'id':  -> card_id, 
+    'plays':  -> default 0, then updated from Recently_Played_Cards_V1 when archived  ---> change this?                                  
+    'from_tel':
+    'to_tel': 
+    'sent_at': -> time.time() when made and stored in S3 and referenced in post_office     
+    'image_url': 
+    'audio_url': 
+    'profile_url': ->        at time postcard was made!  Viewer may see something different
 
 
     
-f'keys_{from_tel}':
-    key -> value, expire -> time.time of expiration
+f'keys_{from_tel}':             # Used for 'connect' for security and to identify the to_tel and avoid having to type that.
+    'version': -> start at 1
+    'key': -> value
+    'to_tel': ->
+    'expire': -> time.time of expiration
 
 """
--- postmaster/views.py
+++ postmaster/views.py
@@ -1,4 +1,113 @@
 """Read SQS to get new postcards, update AWS and update the data structure for the recipient web page"""
 
+import time
+import uuid
+
 from filer import views as filerviews
+from filer import lines
+
+
+
+def first_postcard(from_tel, to_tel, **msg):
+    """Creates the sender with nothing in connections"""
+    wip = msg['wip']
+    profile_url = msg['profile_url']
+    sender = dict(
+        version = 1,
+        from_tel = from_tel,
+        profile_photo_url = profile_url,
+        name = 'from_tel derived',                       # Change by <from: name> command
+        heard_from = time.time(),
+        conn = {}
+    )
+    save_sender(sender)
+    new_postcard(from_tel, to_tel, **msg)
+    delete_twilio_new_sender(sender)
+
+
+def new_postcard(from_tel, to_tel, **msg):
+    """ Writes the postcard,  updates sender, updates the pobox if there is one """
+    wip = msg['wip']                
+    sender = get_sender(from_tel)
+    profile_photo_url = sender['profile_photo_url']
+    card_id, sent_at = make_card_entry(from_tel, to_tel, wip, profile_photo_url)
+    sender['heard_from'] = sent_at
+    conn = sender['conn']
+    if to_tel in conn:              # This an existing connection, so just update the card information
+        pobox_id = conn[to_tel]['pobox_id']
+        conn[to_tel]['recent_card_id'] = card_id
+        conn[to_tel]['recent_card_when'] = sent_at
+        update_pobox(pobox_id, from_tel, card_id)
+    else:
+        conn[to_tel] = dict(
+            to =  'kith or kin',                    # Used by twilio to customize sms to sender
+            po_box_id = None,                           # None until viewer is connected for (from_tel, to_tel). 
+            recent_card_id = card_id,
+            recent_card_when = sent_at
+        )
+        conn['from'] = 'from_tel derived'              # Added explicitly, not in dict(...) because 'from' is a reserved word
+        msg = 'Sender {from_tel} using new to_tel {to_tel}.'
+        new_postcard_msg = lines.line(msg, from_tel=from_tel, to_tel=to_tel)
+        filerviews.nq_admin_message(new_postcard_msg)
+    save_sender(sender)
+
+        
+def make_card_entry(from_tel, to_tel, wip, profile_photo_url):
+    card_id = str(uuid.uuid4())
+    sent_at = time.time()  
+    card = dict(
+        version = 1,
+        id = card_id, 
+        plays = 0,                                  
+        from_tel = from_tel,
+        to_tel = to_tel,
+        sent_at = sent_at,    
+        image_url = wip['image_url'],
+        audio_url = wip['audio_url'],
+        profile_url = profile_photo_url       #  Viewer may see something different
+    )
+    save_card(card)
+    return card_id, sent_at
+
+
+def update_pobox(pobox_id, from_tel, card_id):
+    """contains a list of viewable cards, newest appended"""
+    pobox = get_pobox(pobox_id)    # pobox is created when a viewer is first made. pobox_id is found in sender.
+    pobox[from_tel].append(card_id)
+    save_pobox(pobox)
+
+
+    
+def save_card(card):
+    filerviews._save_a_thing_using_key(thing=card, key=f'card_{"id"}')
+
+    
+def get_sender(from_tel):
+    return filerviews._load_a_thing_using_key(key=f'sender/{from_tel}')
+
+def save_sender(sender):
+    filerviews._save_a_thing_using_key(thing=sender, key=f'sender/{sender["from_tel"]}')
+    save_twilio_free_tier(sender)     # Changes to sender may require changes to be read by twilio
+
+
+
+def get_pobox(pobox_id):
+    return filerviews._load_a_thing_using_key(key=f'pobox_{pobox_id}')
+
+def save_pobox(pobox):
+    filerviews._save_a_thing_using_key(thing=pobox, key=f'pobox_{pobox["id"]}')
+
+    
+def save_twilio_free_tier(sender):      #  Called by save_sender
+    """This a read-only, limited-info entry for twilio processing."""
+    morsel =  sender['conn'].copy()
+    for this_connection in morsel.values():     # Want to leave 'from' and 'to' as only keys
+        for key in this_connection:
+            if key not in ('from', 'to'):
+                this_connection.pop(key)
+    filerviews._save_a_thing_using_key(thing=morsel, key=f'free_tier/{sender["from_tel"]}')
+
+
+def delete_twilio_new_sender(sender):
+    filerviews._delete_a_thing_using_key(key=f'new_sender/{sender["from_tel"]}')
 
