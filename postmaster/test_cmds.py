a28ef2d1a0778fc66cb6c5c2250efdeaad033745
Steve Russell <steven_russell@hotmail.com>
2022-12-08 07:29:11 -0800

    Passing test of new_recipient in test_cmds.

--- postmaster/test_cmds.py
+++ postmaster/test_cmds.py
@@ -12,15 +12,14 @@ from . import cmds, saveget, postcards, connects
 class TwiSim:
     def __init__(self, name):
         self.mobile = f'mobile_{name}'
-        self.twilnumber0 = f'twil0_{name}'
-        self.twilnumber1 = f'twil1_{name}'
+        self.twi_directory = dict(twilnumber0=f'twil0_{name}', twilnumber1=f'twil1_{name}')
         self.profile_url = f'profile_{name}'
         self.url_count = 0
 
-    def _cmd_common(self, **message):
+    def _cmd_common(self, twinumber, **message):
         message['sent_at'] = time.time()
         message['from_tel'] = self.mobile
-        message['to_tel'] = self.twilnumber0
+        message['to_tel'] = self.twi_directory[twinumber]
         return message
 
     def _make_wip(self):
@@ -30,30 +29,30 @@ class TwiSim:
         ++self.url_count
         return wip
 
-    def _new_postcard_common(self):
-        postcard_common = self._cmd_common()
+    def _new_postcard_common(self, twinumber):
+        postcard_common = self._cmd_common(twinumber)
         postcard_common['cmd'] = 'new_postcard'
         postcard_common['wip'] = self._make_wip()
         return postcard_common
 
-    def newsender_firstpostcard(self):
-        sqs_message = self._new_postcard_common()
+    def newsender_firstpostcard(self, twinumber='twilnumber0'):
+        sqs_message = self._new_postcard_common(twinumber)
         sqs_message['context'] = 'NewSenderFirst'
         sqs_message['profile_url'] = self.profile_url
         return json.dumps(sqs_message)
 
-    def newrecipient_postcard(self):
-        sqs_message = self._new_postcard_common()
+    def newrecipient_postcard(self, twinumber='twilnumber0'):
+        sqs_message = self._new_postcard_common(twinumber)
         sqs_message['context'] = 'NewRecipientFirst'
         return json.dumps(sqs_message)
 
-    def newpostcard_noviewer(self):
-        sqs_message = self._new_postcard_common()
+    def newpostcard_noviewer(self, twinumber='twilnumber0'):
+        sqs_message = self._new_postcard_common(twinumber)
         sqs_message['context'] = 'NoViewer'
         return json.dumps(sqs_message)
 
-    def newpostcard_haveviewer(self):
-        sqs_message = self._new_postcard_common()
+    def newpostcard_haveviewer(self, twinumber='twilnumber0'):
+        sqs_message = self._new_postcard_common(twinumber)
         sqs_message['context'] = 'HaveViewer'
         return json.dumps(sqs_message)
 
@@ -89,21 +88,21 @@ class OneCmdTests(TestCase):
         sqs_message = Sender0.newsender_firstpostcard()
         res = cmds.interpret_one_cmd(sqs_message)
         sender = saveget.get_sender(Sender0.mobile)
-        self.assertEqual(sender['profile_url'], 'profile_Mr.0')
+        self.assertEqual(sender['profile_url'], 'profile_Mr0')
 
     def test_using_simulation_of_two_senders(self):
-        Sender0 = TwiSim('Mr.0')
-        Sender1 = TwiSim('Ms.1')
+        Sender0 = TwiSim('Mr0')
+        Sender1 = TwiSim('Ms1')
         # Twilio side is quiet until the sender succeeds in 'sign-up' by making wip and a profile.
         # Sender0 and Sender1 sign up, neither has a viewer yet. Nothing new being tested here
         res0 = cmds.interpret_one_cmd(Sender0.newsender_firstpostcard())
         res1 = cmds.interpret_one_cmd(Sender1.newsender_firstpostcard())
         # Sender1 sends to a second twilio number without establishing any viewer
-        #    ????????? How to write this second to_tel  ?????
-        # res1 = cmds.interpret_one_cmd(Sender0.newrecipient_postcard())
+        res2 = cmds.interpret_one_cmd(Sender1.newrecipient_postcard('twilnumber1'))
+        twilnumber1 = Sender1.twi_directory['twilnumber1']
         sender1 = saveget.get_sender(Sender1.mobile)
-        self.assertEqual(sender1['profile_url'], 'profile_Ms.1')
-        self.assertFalse('I do not know how to write the second to_tel')
+        self.assertEqual(sender1['profile_url'], 'profile_Ms1')
+        self.assertIn(twilnumber1, sender1['conn'])
 
 
 
