c796ea0af08d3ad6d8ac160b2e22ce7fb54bbd0c
Steve Russell <steven_russell@hotmail.com>
2022-12-16 13:33:50 -0800

    Modified a player.test to keep up with changed urls.

--- player/tests.py
+++ player/tests.py
@@ -13,8 +13,8 @@ class LookAtPagesAndViews(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_to_connect_page_is_reachable(self):
-        response = self.client.get('/connect')
+    def test_get_a_pobox_id_page_is_reachable(self):
+        response = self.client.get('/get_a_pobox_id')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'to_connect.html')
 
@@ -30,7 +30,7 @@ class LookAtPagesAndViews(TestCase):
 
     def test_viewer_data_is_HTTP_fetchable(self):
         data_source = os.environ['POSTBOX_DATA_SOURCE']
-        response = requests.get(f'{data_source}/viewer_data/some_postbox_id')
+        response = requests.get(f'{data_source}/viewer_data/test_pobox')
         viewer_data = json.loads(response.content)
         self.assertEqual(response.status_code, 200)
         self.assertIn('meta', viewer_data)
