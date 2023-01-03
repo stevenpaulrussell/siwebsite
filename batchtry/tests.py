828f86880356d6ecfc44f0fb5e066d775d06d193
Steve <steven_russell@hotmail.com>
2023-01-03 15:00:03 -0800

    Second commit, should explicitly write sender, use free_tier for postcards and connections.

--- batchtry/tests.py
+++ batchtry/tests.py
@@ -1,19 +1,18 @@
 
 import os
 import json
+import time
 
-import requests
 from django.test import TestCase
 
-from twitalk import views
+from twitalk import free_tier
 
 data_source = os.environ['POSTBOX_DATA_SOURCE']
 
 
 class OneCmdTests(TestCase):
     def test_simple(self):
-        data = dict(From='a from tel', To='a twilio number', Body='help')
-        response = requests.post(f'{data_source}/twitalk/accept_media', data=(data))
-        self.assertEqual(response.status_code, 200)
-        print(f'response: {response.text}')
+        data = dict(timestamp= time.time(), from_tel='from_tel', to_tel='to_tel', text='help', image_url='image_url', free_tier_morsel={})
+        from_tel_msg = free_tier.mms_to_free_tier(**data)
+        print(f'from_tel_msg: {from_tel_msg}')
 
