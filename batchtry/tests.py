70cf37a3764bf3696305d1b4741c138c3652cca6
Steve Russell <steven_russell@hotmail.com>
2023-01-06 17:11:46 -0800

    Reading state and cards from old system.

--- batchtry/tests.py
+++ batchtry/tests.py
@@ -2,17 +2,56 @@
 import os
 import json
 import time
+import pprint
 
 from django.test import TestCase
 
-from twitalk import free_tier
+from twitalk import new_sender, free_tier
+from postoffice import cmds
+
+from . import views as old_filer
 
 data_source = os.environ['POSTBOX_DATA_SOURCE']
+wanted_from_tels = ('+16502196500', '+16502185923', '+16508159597', '+12068173605')
+
+pp = pprint.PrettyPrinter(indent=2)
+
+# Get postcards ordered by time.
+# Get senders ordered by from_tel
+# Get connections ordered by from_tel
+
+# 1. From a list of wanted senders, for each sender, get oldest card and profile and nq the sender.
+# 2. Connect the 650.219... to the established pobox_id, adding a key value to do this into connects.connect_viewer
+# 3. For each of the others, run connect command by getting passkey in twitalk (doing nq write and read) and...
+#       running connect command, sent from free_tier.
+#  ==> write this as a functional test, which it is, but produces a useful result. After each step, test and
+#       maybe print for inspection.
+#       Write all this in test_  !!
+#
+
 
 
 class OneCmdTests(TestCase):
-    def test_simple(self):
-        data = dict(timestamp= time.time(), from_tel='from_tel', to_tel='to_tel', text='help', image_url='image_url', free_tier_morsel={})
-        from_tel_msg = free_tier.mms_to_free_tier(**data)
-        print(f'from_tel_msg: {from_tel_msg}')
+    def setUp(self) -> None:
+        return super().setUp()
 
+    def test_build_one_step_at_a_time(self):
+        old_filer.aws_read_bucket_name = 'real-software-bucket'
+        all_state = old_filer.load_state_from_s3()
+        senders, post_office, cards_in_progress, connections, postal_admin = all_state
+        some_cards = old_filer.get_cards(limit=4)
+        print('\n')
+        print('\nsenders')
+        for from_tel in senders:
+            print('\n')
+            pp.pprint(senders[from_tel])
+        print('\n')
+        print('\nconnections')
+        for from_tel in connections:
+            print('\n')
+            pp.pprint(connections[from_tel])
+        print('\n')
+        print(f'got {len(some_cards)}')        
+        for a_card in some_cards:
+            print('\n')
+            pp.pprint(a_card)
--- batchtry/views.py
+++ batchtry/views.py
@@ -1,3 +1,78 @@
-from django.shortcuts import render
 
-# Create your views here.
+
+import boto3
+import json
+import os
+
+# Production read and write source is an AWS bucket.  
+# Test read and write may be to a 'TEST bucket.
+
+client = boto3.client(
+            's3', 
+            aws_access_key_id=os.environ['AWSAccessKeyId'], 
+            aws_secret_access_key=os.environ['AWSSecretKey']
+            )
+
+if os.environ['TEST'] == 'True':    
+    TEST = True
+    aws_read_bucket_name = os.environ['TEST_BUCKET']        # This from .env  Not from Heroku config
+    aws_write_bucket_name = os.environ['TEST_BUCKET']  
+elif os.environ['TEST'] == 'Beta':
+    TEST = 'Beta'
+    aws_read_bucket_name = os.environ['BETA_BUCKET']        # From Heroku config
+    aws_write_bucket_name = os.environ['BETA_BUCKET']  
+else:
+    TEST = False
+    assert os.environ['TEST'] == 'False'
+    aws_read_bucket_name = os.environ['ALPHA_BUCKET']       # From Heroku config
+    aws_write_bucket_name = os.environ['ALPHA_BUCKET']
+
+def load_state_from_s3():
+    all_data = _load_a_thing_using_key('All_Data_Test_v1')
+    if all_data:
+        senders = all_data['senders']
+        post_office = all_data['post_office']
+        cards_in_progress = all_data['cards_in_progress']
+        connections = all_data['connections']
+        postal_admin = all_data['postal_admin']
+        return senders, post_office, cards_in_progress, connections, postal_admin
+    else:
+        return {}, {}, {}, {}, {}
+
+def load_recently_played_card_list_from_s3():
+    try:
+        return _load_a_thing_using_key('Recently_Played_Cards_v1')
+    except(Exception) as E:
+        if TEST:            # If running Beta or Test, not an error.
+            return {}
+        else:
+            raise
+
+def load_recently_played_card_list_as_last_seen_from_s3():
+    try:
+        return _load_a_thing_using_key('Recently_Played_Cards_v1_as_last_seen')
+    except(Exception) as E:
+        return {}
+
+def load_a_postcard_from_s3(postcard_uuid):
+    return _load_a_thing_using_key(thing_key=f'cards/{postcard_uuid}')
+
+
+
+# Utility functions
+def _load_a_thing_using_key(thing_key):
+    response_dictionary = client.get_object(Bucket=aws_read_bucket_name, Key=thing_key)
+    return json.loads(response_dictionary['Body'].read())
+
+def get_cards(limit=1000):
+    all_cards = []
+    count = 0
+    result_dictionary = client.list_objects_v2(Bucket = aws_read_bucket_name, Prefix='cards/')
+    for item in result_dictionary.get('Contents'):
+        count += 1
+        key = item.get('Key')
+        card = _load_a_thing_using_key(key)
+        all_cards.append(card)
+        if count == limit:
+            break
+    return all_cards
