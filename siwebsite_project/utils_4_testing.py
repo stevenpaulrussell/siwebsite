0ee1906c9309cb325cbba24d5f7f0b32ea18e3be
Steve <steven_russell@hotmail.com>
2023-09-08 16:30:18 -0700

    Have first simple test passing with New_Tests_Sender object to clean up tests.

--- siwebsite_project/utils_4_testing.py
+++ siwebsite_project/utils_4_testing.py
@@ -12,11 +12,32 @@ class make_sender_values():
             audio_url = self.audio_url
         )
 
-class MintaSender():
-    def __init__(self, tel_id_number='1', svc_id_letter='A'):
-        self.tel_id = f'+{tel_id_number}'
+
+
+import time
+
+class New_Tests_Sender():
+    """This intended to simplify test by simpler naming and simple methods."""
+    last_tel_id_number = 0
+    last_card_number = 0
+
+    def __init__(self, svc_id_letter='A'):
+        New_Tests_Sender.last_tel_id_number += 1
+        self.tel_id = f'+ab{New_Tests_Sender.last_tel_id_number}'
         self.svc_id = f'+{svc_id_letter}'
-        self.profile_url = f'{tel_id_number}_pro'
-        self.sender_moniker = self.tel_id
+        self.profile_url = f'{self.tel_id}_profile'
+
+    def new_card_wip(self, now = time.time()):
+        New_Tests_Sender.last_card_number += 1
+        card_id = f'c_{New_Tests_Sender.last_card_number}'
+        return dict(
+            image_timestamp = now, 
+            image_url = f'c_{card_id}_img', 
+            audio_timestamp = now, 
+            audio_url = f'c_{card_id}_aud'
+        )
+
+
+
 
             
